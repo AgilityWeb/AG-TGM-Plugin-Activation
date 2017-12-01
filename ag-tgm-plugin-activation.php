7b93480833d9afae43caa571030d3c9a3876887b
AgilityWeb <mail@agilityweb.co.uk>
2017-12-01 15:40:55 +0000

    Create ag-tgm-plugin-activation.php

---
+++ ag-tgm-plugin-activation.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: AG TGM Plugin Activation
+Plugin URI: http://agilityweb.co.uk
+Description: Initial suite of plugins to install
+Version: 1.0
+Author: Caspar Kennerdale
+Author URI: http://agilityweb.co.uk
+License: GPLv2 or later
+Text Domain: ag-tgm-plugin-activation
+*/
+
+
+ 
+
+require plugin_dir_path( __FILE__ ) . '/inc/class-tgm-plugin-activation.php';
+require plugin_dir_path( __FILE__ ) . '/inc/required-plugins.php';
+
+?>
